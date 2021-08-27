11da08442f44b1de712766ebf271def383982bcd
anupama cv <anu.anupamacv@gmail.com>
2021-08-28 00:48:52 +0530

    Added: functionality to add collaberator in add note section

--- js/google_keep.js
+++ js/google_keep.js
@@ -1,6 +1,7 @@
 // import {deleteNote} from './service.js';
 
 let collabList=[];
+let searchResults=[]
 
 const headerconfig = {
   headers: { 'Content-Type': 'application/json', 
@@ -35,8 +36,8 @@ var title = document.getElementById("toggle");
 function insert() {
 
   let data = {"title": title.value};
-    data["description"] = note.value
-    data[""]
+    data["description"] = note.value;
+    data["collaborator"] = collabList;
     axios.post("http://fundoonotes.incubation.bridgelabz.com/api/notes/addNotes",
     data, headerconfig 
   )
@@ -55,16 +56,22 @@ function getNotes() {
   headerconfig 
   )
   .then(res=> {
-    console.log(res.data)
+    // console.log('anupama')
     var nHTML = '';
     for(let i=0; i<res.data.data.data.length; i++) {
       if(res.data.data.data[i].isDeleted == false) {
+      let colString ="";
+      for(let j=0; j<res.data.data.data[i].collaborators.length; j++){
+        colString += res.data.data.data[i].collaborators[i].email, ", ";
+      }
       nHTML += `<div class="item-container">
       <div class="items"> 
         <li style="list-style-type:none">` + res.data.data.data[i].title + " "+
         `</li>` + 
         `<li style="list-style-type:none">` + res.data.data.data[i].description + 
         `</li>` + 
+        `<li style="list-style-type:none">` + colString +
+        `</li>`+
         `<button id=`+ res.data.data.data[i].id +` type="button" onclick="trashNote(id)">Delete
         </button>
         <div class="sub-buttons">
@@ -97,10 +104,10 @@ function search() {
       data, headerconfig  
     )
     .then(res=> { 
-      var array = res.data.data.details;
+      searchResults = res.data.data.details;
+      // searchResults = array;
       for(let i=0; i< res.data.data.details.length; i++) {
-        console.log(array[i])
-        nHTML += ` <li style="list-style-type:none"><div id="`+ res.data.data.details[i].email +`" onclick=addToCollabaratorList(id) >` + res.data.data.details[i].email+
+        nHTML += ` <li style="list-style-type:none"><div id="`+ i +`" onclick=addToCollabaratorList(id) >` + res.data.data.details[i].email+
           `</div> </li>`;
       }
       document.getElementById("colab-list").innerHTML = '<ul>' + nHTML + '</ul>' ;
@@ -123,21 +130,22 @@ function addCollaborator() {
 
 function addNoteCollaborator() {
   switchVisible();
-  let email = document.getElementById("search-email").value;
   displayCollabListInMain();
 }
 
 
-function addToCollabaratorList(selectedEmail){
-  collabList.push(selectedEmail)
-  console.log(selectedEmail);
+function addToCollabaratorList(i){
+  selectedEmail = searchResults[i].email;
+  // let firstChar = selectedEmail.charAt[0];
+  collabList.push(searchResults[i])
+  console.log(searchResults[i]);
 }
 
 function displayCollabListInMain(){
   var colab  = document.getElementById("addnote-collab-h");
   let val ="";
   for(let i=0; i< collabList.length; i++){
-    val += collabList[i] + ', ';
+    val += collabList[i].email + ', ';
   }
   colab.innerHTML = val;
 }
