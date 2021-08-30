582b16739859868de4d3fe32d44560adfd76e5a5
anupama cv <anu.anupamacv@gmail.com>
2021-08-30 18:38:24 +0530

    Added: functionality to create color pallet in add note section and also included archive and color in addnote api call

--- js/google_keep.js
+++ js/google_keep.js
@@ -1,7 +1,8 @@
-// import {deleteNote} from './service.js';
-
 let collabList=[];
-let searchResults=[]
+let searchResults=[];
+let displayColabList=[];
+let notesList = [];
+// let archive = false;
 
 const headerconfig = {
   headers: { 'Content-Type': 'application/json', 
@@ -32,17 +33,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 var note = document.getElementById("user-note");
 var title = document.getElementById("toggle");
+var archive =document.getElementById("archive-button");
 
 function insert() {
-
+  let rgb = document.getElementById("note-section").style.backgroundColor;
   let data = {"title": title.value};
     data["description"] = note.value;
-    data["collaborator"] = collabList;
+    data["collaberators"] = collabList;
+    data["isArchived"] = archive;
+    data["color"] = '#' + rgb.slice(4,-1).split(',').map(x => (+x).toString(16).padStart(2,0)).join('')
     axios.post("http://fundoonotes.incubation.bridgelabz.com/api/notes/addNotes",
     data, headerconfig 
   )
   .then(res=> {
-  console.log(res+"-----------");
   console.log(res.data);
   })
   getNotes();
@@ -56,36 +59,39 @@ function getNotes() {
   headerconfig 
   )
   .then(res=> {
-    // console.log('anupama')
     var nHTML = '';
+    notesList = res.data.data.data;
     for(let i=0; i<res.data.data.data.length; i++) {
       if(res.data.data.data[i].isDeleted == false) {
-      let colString ="";
-      for(let j=0; j<res.data.data.data[i].collaborators.length; j++){
-        colString += res.data.data.data[i].collaborators[i].email, ", ";
-      }
-      nHTML += `<div class="item-container">
-      <div class="items"> 
-        <li style="list-style-type:none">` + res.data.data.data[i].title + " "+
-        `</li>` + 
-        `<li style="list-style-type:none">` + res.data.data.data[i].description + 
-        `</li>` + 
-        `<li style="list-style-type:none">` + colString +
-        `</li>`+
-        `<button id=`+ res.data.data.data[i].id +` type="button" onclick="trashNote(id)">Delete
-        </button>
-        <div class="sub-buttons">
-          <i class="fa fa-bell-o" aria-hidden="true"></i>
-          <button id="Button1" class="collaborator-button" value="Click" onclick="switchVisible()"/>
-            <i class="fa fa-user-plus" aria-hidden="true">
-            </i>
-          </button> 
-          <i class="fa fa-picture-o" aria-hidden="true"></i>
-          <i class="fa fa-archive" aria-hidden="true"></i>
-          <i class="fa fa-ellipsis-v" aria-hidden="true"></i> 
-          </div>
-        </div>
-      </div>`;
+        let colString ="";
+        for(let j=0; j<res.data.data.data[i].collaborators.length; j++){
+          colString += res.data.data.data[i].collaborators[i].email, " ,";
+        }
+        nHTML += `<div class="item-container" >
+                    <div class="items" style="background-color:`+res.data.data.data[i].color+`">                                       
+                      <button class="s3-btn" name="Open" style="background-color:`+res.data.data.data[i].color+`" id=`+i+` onclick="popupOpen(id);">
+                        <li style="list-style-type:none">` + res.data.data.data[i].title + " "+
+                        `</li>` + 
+                        `<li style="list-style-type:none">` + res.data.data.data[i].description + 
+                        `</li>` + 
+                        `<li style="list-style-type:none">` + colString +
+                        `</li>` + 
+                      `</button>
+                      <div class="sub-buttons">
+                        <i class="fa fa-bell-o" aria-hidden="true"></i>
+                        <button id="Button1" class="collaborator-button" value="Click" onclick="switchVisible()">
+                          <i class="fa fa-user-plus" aria-hidden="true">
+                          </i>
+                        </button>
+                        <i class="fa fa-picture-o" aria-hidden="true"></i>
+                        <i class="fa fa-archive" aria-hidden="true"></i>
+                        <i class="fa fa-ellipsis-v" aria-hidden="true"></i> 
+                        <button id=`+ res.data.data.data[i].id +` type="button" class="delete-buttton" onclick="trashNote(id)">Delete
+                        </button>
+                      </div>
+                    </div>                      
+                  </div>                                
+                `;
       }
     }
     document.getElementById("item-list").innerHTML = '<ul>' + nHTML + '</ul>' ;
@@ -105,7 +111,6 @@ function search() {
     )
     .then(res=> { 
       searchResults = res.data.data.details;
-      // searchResults = array;
       for(let i=0; i< res.data.data.details.length; i++) {
         nHTML += ` <li style="list-style-type:none"><div id="`+ i +`" onclick=addToCollabaratorList(id) >` + res.data.data.details[i].email+
           `</div> </li>`;
@@ -118,7 +123,6 @@ function search() {
 // add collaborator in collaborator page method
 
 function addCollaborator() {
-  var x = 0;
   let email = document.getElementById("search-email").value;
   console.log(email);
   var nHTML = '';
@@ -133,19 +137,63 @@ function addNoteCollaborator() {
   displayCollabListInMain();
 }
 
-
 function addToCollabaratorList(i){
-  selectedEmail = searchResults[i].email;
-  // let firstChar = selectedEmail.charAt[0];
   collabList.push(searchResults[i])
-  console.log(searchResults[i]);
+  let selectedEmail = searchResults[i].email;
+  displayColabList.push(selectedEmail[0]);
 }
 
 function displayCollabListInMain(){
   var colab  = document.getElementById("addnote-collab-h");
   let val ="";
-  for(let i=0; i< collabList.length; i++){
-    val += collabList[i].email + ', ';
+  for(let i=0; i< displayColabList.length; i++){
+    val += displayColabList[i] + '   ';
   }
   colab.innerHTML = val;
 }
+
+function isArchive() {
+  archive = !archive;
+}
+
+isArchive();
+
+// display note section popup Open method
+
+function popupOpen(i){
+  let selectedItem = notesList[i];
+  console.log(selectedItem)
+  document.getElementById("popup").style.display="block";
+  document.getElementById("overlay").style.display="block";
+  var nHTML = '';
+  nHTML += `                                                             
+    <button class="s3-btn" name="Open" style="background-color:`+selectedItem.color+`" id=`+i+` onclick="popupOpen(id);">
+      <li style="list-style-type:none">` + selectedItem.title + " "+
+      `</li>` + 
+      `<li style="list-style-type:none">` + selectedItem.description + 
+      `</li>` + 
+      
+    `</button>
+    <div class="sub-buttons">
+      <i class="fa fa-bell-o" aria-hidden="true"></i>
+      <button id="Button1" class="collaborator-button" value="Click" onclick="switchVisible()">
+        <i class="fa fa-user-plus" aria-hidden="true">
+        </i>
+      </button>
+      <i class="fa fa-picture-o" aria-hidden="true"></i>
+      <i class="fa fa-archive" aria-hidden="true"></i>
+      <i class="fa fa-ellipsis-v" aria-hidden="true"></i> 
+      <button id=`+ selectedItem.id +` type="button" class="delete-buttton" onclick="trashNote(id)">Delete
+      </button>
+    </div>                  
+  `
+  document.getElementById("popup-inner-content").innerHTML = nHTML;  
+}
+
+// display note section Popup Close method
+
+function popupClose(){
+  document.getElementById("popup").style.display="none";
+  document.getElementById("overlay").style.display="none";
+}
+
--- js/icons.js
+++ js/icons.js
@@ -4,7 +4,30 @@ document.getElementById("icons").innerHTML =`
     <i class="fa fa-user-plus" aria-hidden="true">
     </i>
     </button>
+
+    <div class="btn-group dropup" id="color-palette-dropdown">
+        <button type="button" id="btn-colors" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="fa fa-picture-o" aria-hidden="true"></i>
+        </button>
+        <div class="color-palette dropdown-menu" id ="color-palette">
+            <div class="bg-white circled"></div>
+            <div class="bg-red"></div>
+            <div class="bg-orange"></div>
+            <div class="bg-yellow"></div>
+            <div class="bg-green"></div>
+            <div class="bg-turquoise"></div>
+            <div class="bg-blue"></div>
+            <div class="bg-dark-blue"></div>
+            <div class="bg-purple"></div>
+            <div class="bg-pink"></div>
+            <div class="bg-brown"></div>
+            <div class="bg-grey"></div>
+        </div>
+    </div>
     <i class="fa fa-picture-o" aria-hidden="true"></i>
+    <button class="archive-button" id="archive-button" onclick="isArchive()">
+        <i class="fa fa-picture-o" aria-hidden="true"></i>       
+    </button>
     <i class="fa fa-archive" aria-hidden="true"></i>
     <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
     <i class="fa fa-undo" aria-hidden="true"></i>
